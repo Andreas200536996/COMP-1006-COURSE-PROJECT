f2181bb17ab495fe4dd2a9ac5554448146c7b9cc
Andreas200536996 <113151311+Andreas200536996@users.noreply.github.com>
2023-04-04 16:56:22 -0400

    Initial Commit

---
+++ app.sql
@@ -0,0 +1,24 @@
+CREATE DATABASE IF NOT EXISTS comp_1006_200536996;
+USE comp_1006_200536996;
+
+-- YOU MUST USE THIS TABLE AS IS (or at least the 3 defined fields name, email, and password)
+CREATE TABLE users (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(50) NOT NULL,
+    email VARCHAR(200) NOT NULL,
+    password VARCHAR(256) NOT NULL
+);
+
+CREATE TABLE restaurant (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    restaurant_name VARCHAR(50) NOT NULL
+);
+
+CREATE TABLE restaurant_reservations (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    parent_id INT NOT NULL,
+    customer_name varchar(50) not null,
+    reservation_time time not null,
+    reservation_date date not null,
+    FOREIGN KEY (parent_id) REFERENCES parent_resource (id) ON DELETE CASCADE
+);
